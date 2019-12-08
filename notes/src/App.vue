6a5794243a4208e4a52d7d9a3bd17f44c632425f
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-12-08 09:18:14 +0400

    add notes component
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- notes/src/App.vue
+++ notes/src/App.vue
@@ -3,23 +3,52 @@
     <div class="wrapper-content">
       <section>
         <div class="container">
-          <h1>{{ title }}</h1>
-
           <message v-if="message" :message="message" />
-
           <newNote :note="note" @addNote="addNote" />
-
-          <div class="notes">
-            <div class="note" v-for="(note, index) in notes" :key="index">
-              <div class="note-header">
-                <p>{{ note.title }}</p>
-              </div>
-              <div class="note-body">
-                <p>{{ note.descr}}</p>
-                <span>{{ note.date }}</span>
-              </div>
+          <div class="note-header">
+            <h1>{{ title }}</h1>
+            <div class="icons">
+              <svg
+                :class="{ active: grid }"
+                @click="grid = true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <rect x="3" y="3" width="7" height="7" />
+                <rect x="14" y="3" width="7" height="7" />
+                <rect x="14" y="14" width="7" height="7" />
+                <rect x="3" y="14" width="7" height="7" />
+              </svg>
+              <svg
+                :class="{ active: !grid }"
+                @click="grid = false"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                stroke-width="2"
+                stroke-linecap="round"
+                stroke-linejoin="round"
+              >
+                <line x1="8" y1="6" x2="21" y2="6" />
+                <line x1="8" y1="12" x2="21" y2="12" />
+                <line x1="8" y1="18" x2="21" y2="18" />
+                <line x1="3" y1="6" x2="3" y2="6" />
+                <line x1="3" y1="12" x2="3" y2="12" />
+                <line x1="3" y1="18" x2="3" y2="18" />
+              </svg>
             </div>
           </div>
+          <notes :notes="notes" @remove="removeNote" :grid="grid" />
         </div>
       </section>
     </div>
@@ -29,17 +58,20 @@
 <script>
 import message from '@/components/Message.vue'
 import newNote from '@/components/NewNote.vue'
+import notes from '@/components/Notes.vue'
 
 export default {
   name: 'app',
   components: {
     message,
     newNote,
+    notes
   },
   data () {
     return {
       title: 'Notes App',
       message: null,
+      grid: true,
       note: {
         title: '',
         descr: ''
@@ -82,10 +114,34 @@ export default {
       this.note.title = '';
       this.note.descr = '';
       this.message = null;
+    },
+    removeNote (index) {
+      this.notes.splice(index, 1)
     }
   }
 }
 </script>
 
 <style lang="scss">
+.note-header {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  h1 {
+    font-size: 32px;
+  }
+  svg {
+    margin-right: 12px;
+    color: #999999;
+    &.active {
+      color: #402caf;
+    }
+    &:last-child {
+      margin-right: 0;
+    }
+    &:hover {
+      cursor: pointer;
+    }
+  }
+}
 </style>
