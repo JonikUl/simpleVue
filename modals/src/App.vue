5c5968f6919cace96ca9710a090501f5c5d27c13
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-12-09 17:49:15 +0400

    modals initial
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

---
+++ modals/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="wrapper">
+    <div class="wrapper-content">
+      <section>
+        <div class="container">
+          <img
+            alt="Vue logo"
+            src="./assets/logo.png"
+          >
+        </div>
+      </section>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+}
+</script>
+
+<style lang="scss">
+</style>
