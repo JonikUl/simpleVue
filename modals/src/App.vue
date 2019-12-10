1912ee1c51a4cffd6a755977d0a4f6ca9b30b440
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-12-10 07:43:21 +0400

    add modal form
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- modals/src/App.vue
+++ modals/src/App.vue
@@ -4,10 +4,34 @@
       <section>
         <div class="container">
           <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Show first modal</button>
-          <modals title="First modal" v-show="modalFirst" @close="modalFirst = !modalFirst">
+          <modals title="First modal" v-show="modalFirst" @close="modalFirst = false">
             <div slot="body">
               <p>Text</p>
               <button class="btn btnPrimary" @click="modalFirst = !modalFirst">Well Done!</button>
+            </div>
+          </modals>
+
+          <button
+            class="btn btnPrimary"
+            @click="modalSecond.show = !modalSecond.show"
+          >Show modal with form</button>
+          <modals
+            title="Modal with form"
+            v-show="modalSecond.show"
+            @close="modalSecond.show = false"
+          >
+            <div slot="body">
+              <form @submit.prevent="submitSecondForm">
+                <label>
+                  Name:
+                  <input type="text" v-model="modalSecond.name" required />
+                </label>
+                <label>
+                  Email:
+                  <input type="email" v-model="modalSecond.email" required />
+                </label>
+                <button class="btn btnPrimary">Submit!</button>
+              </form>
             </div>
           </modals>
         </div>
@@ -22,11 +46,30 @@ export default {
   components: { modals },
   data () {
     return {
-      modalFirst: false
+      modalFirst: false,
+      modalSecond: {
+        show: false,
+        name: '',
+        email: '',
+      }
+    }
+  },
+  methods: {
+    submitSecondForm () {
+      console.log({
+        name: this.modalSecond.name,
+        email: this.modalSecond.email,
+      });
+      this.modalSecond.name = '';
+      this.modalSecond.email = '';
+      this.modalSecond.show = false;
     }
   }
 }
 </script>
 
 <style lang="scss">
+label {
+  cursor: pointer;
+}
 </style>
