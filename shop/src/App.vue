933539bff19945a7f0cbdc5df349b40bf88e1a5c
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-12-20 07:56:45 +0400

    shop reinit
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,41 +1,23 @@
 <template>
   <div class="wrapper">
-    <header>
-      <div class="navbar">
+    <div class="wrapper-content">
+      <section>
         <div class="container">
-          <div class="navbar-content">
-            <div class="logo">VUE-CLI</div>
-            <ul class="navbar-list">
-              <li class="navbar-item" v-for="link in links" :key="link.title">
-                <router-link :to="link.url" :title="link.title" class="navbar-link">{{ link.title }}</router-link>
-              </li>
-            </ul>
-          </div>
+          <img
+            alt="Vue logo"
+            src="./assets/logo.png"
+          >
         </div>
-      </div>
-    </header>
-    <router-view></router-view>
+      </section>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'app',
-  data () {
-    return {
-      links: [
-        { title: 'Home', url: '/' },
-        { title: 'Example', url: '/example' },
-      ]
-    }
-  }
 }
 </script>
 
 <style lang="scss">
-.navbar-link {
-  &.router-link-exact-active {
-    color: #5247e7;
-  }
-}
 </style>
