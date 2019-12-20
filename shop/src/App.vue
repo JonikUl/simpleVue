4618d6e12bd96086eece01c593095b04e1010da5
Evgeniy Gromin <evgeniy.gromin@gmail.com>
2019-12-20 08:08:40 +0400

    add main pages
    Signed-off-by: Evgeniy Gromin <evgeniy.gromin@gmail.com>

--- shop/src/App.vue
+++ shop/src/App.vue
@@ -1,23 +1,45 @@
 <template>
   <div class="wrapper">
-    <div class="wrapper-content">
-      <section>
+
+    <header>
+      <div class="navbar">
         <div class="container">
-          <img
-            alt="Vue logo"
-            src="./assets/logo.png"
-          >
+          <div class="navbar-content">
+            <div class="logo">VUE-CLI</div>
+            <ul class="navbar-list">
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link class="navbar-link" :title="link.title" :to="link.url">{{ link.title }}</router-link>
+              </li>
+            </ul>
+          </div>
         </div>
-      </section>
-    </div>
+      </div>
+    </header>
+
+    <router-view></router-view>
+
+    <!-- <footer></footer> -->
+
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app',
+  data () {
+    return {
+      links: [
+        { title: 'Home', url: '/' },
+        { title: 'Shop', url: '/shop' },
+      ]
+    }
+  }
 }
 </script>
 
 <style lang="scss">
+.navbar-link {
+  &.router-link-exact-active {
+    color: #5247e7;
+  }
+}
 </style>
